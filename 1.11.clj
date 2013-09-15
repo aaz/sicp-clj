debec484585cf6cd0585088ee5f169d392c631cf
aaz <akbar.zamir@gmail.com>
2013-09-15 18:21:39 +0100

    Humble beginnings

---
+++ 1.11.clj
@@ -0,0 +1,29 @@
+;;; Exercise 1.11
+;;;   f(n) = n, if n < 3
+;;;   f(n) = f(n-1) + 2f(n-2) + 3f(n-3), if n >= 3
+
+;;; Recursive process for f
+
+(defn f-recur
+  "Computes f(n) using a recursive process"
+  [n]
+  (cond
+   (< n 3) n
+   :else (+ (f-recur (- n 1))
+            (* 2 (f-recur (- n 2)))
+            (* 3 (f-recur (- n 3))))))
+
+;;; Iterative process for f
+
+(defn f
+  [fn-1 fn-2 fn-3 count n]
+  (cond
+   (= count n) (+ fn-1 (* 2 fn-2) (* 3 fn-3))
+   :else (f (+ fn-1 (* 2 fn-2) (* 3 fn-3)) fn-1 fn-2 (+ count 1) n)))
+
+(defn f-iter
+  "Computes f(n) using an iterative process"
+  [n]
+  (cond
+   (< n 3) n
+   :else (f 2 1 0 3 n)))
